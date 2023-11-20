d09571e051dd78bac41ac8a49776f5058be879be
kyrojohnsolo <146120321+kyrojohnsolo@users.noreply.github.com>
2023-11-20 12:55:20 -0500

    Completed animations
    Animations have been added for player characters.

--- PlatformerGameVar2/main.lua
+++ PlatformerGameVar2/main.lua
@@ -30,12 +30,15 @@ function love.load()
     world:addCollisionClass('Danger') -- creates a Danger Collision class
     
 
-    player = world:newRectangleCollider(360,100, 80, 80, {collision_class = "Player"}) -- creates square for player.
+    player = world:newRectangleCollider(360,100, 40, 100, {collision_class = "Player"}) -- creates square for player.
     player:setFixedRotation(true) -- prevents the player object from rotating.
     player.speed = 240 --sets the player speed.
-    player.animation = animations.run -- creates an animation property we can change
-    player.maxJumpCount = 1
-    player.jumpCount = 1
+    player.animation = animations.idle -- creates an animation property we can change
+    player.isMoving = false --used for animations. when true player is running, false is idle
+    player.direction = 1 -- used for direction. flips the player in the draw.update function
+    player.grounded = true -- detects if player is on the ground
+    player.maxJumpCount = 1 -- used for double jumping
+    player.jumpCount = 1 -- used for double jumping.
     platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = "Platform"}) -- creates platform.
     platform:setType('static') -- sets platform to 'static' so it's not impacted by gravity.
 
@@ -46,6 +49,18 @@ end
 function love.update(dt)
     world:update(dt) -- updates the world to run at dt
     if player.body then -- this if statement checks to see if the player object is still in play.
+        
+        --[[
+            This is a collider that checks if player is on the ground.
+        ]]
+        local colliders = world:queryRectangleArea(player:getX() - 20, player:getY() + 50, 40, 2, {'Platform'})
+        if #colliders > 0 then
+            player.grounded = true
+        else
+            player.grounded = false
+        end
+        
+        player.isMoving = false -- sets the player.isMoving to false by default (if no buttons are being pressed).
         local px, py = player:getPosition() -- creates local variable that grabs player position.
         --[[
             Player Movement Logic
@@ -54,21 +69,38 @@ function love.update(dt)
         ]]
         if love.keyboard.isDown('right') then
             player:setX(px + player.speed*dt)
+            player.isMoving = true
+            player.direction = 1
         end
         if love.keyboard.isDown('left') then
             player:setX(px - player.speed*dt)
+            player.isMoving = true
+            player.direction = -1
         end
         if player:enter('Danger') then
             player:destroy()
         end
     end
-
+    
+    --[[
+        Cycles through run, idle or jump depending on user actions.
+    ]]
+    if player.grounded then
+        if player.isMoving then
+            player.animation = animations.run
+        else
+            player.animation= animations.idle
+        end
+    else
+        player.animation = animations.jump
+    end
     player.animation:update(dt) -- cycles animation with dt
 end
 
 function love.draw()
     world:draw() -- draws world on screen.
-    player.animation:draw(sprites.playerSheet, 0, 0) -- draws playersheet to screen.
+    local px, py = player:getPosition() -- creates a local variable for the player position
+    player.animation:draw(sprites.playerSheet, px, py, nil, 0.25 * player.direction, 0.25, 130, 300) -- draws playersheet to screen.
 end
 
 --[[
@@ -80,22 +112,36 @@ end
     -- CUSTOM WHOOOOO HOOOOO
     -- implemented double jump.
 ]]
+
 function love.keypressed(key)
     if key == 'up' then
-        local colliders = world:queryRectangleArea(player:getX() - 40, player:getY() + 40, 80, 2, {'Platform'})
-        if #colliders > 0 and player.jumpCount > 0 then
-            player:applyLinearImpulse(0, -7000)            
+        if player.grounded and player.jumpCount > 0 then
+            player:applyLinearImpulse(0, -4000)
+            player.animation = animations.jump            
         end
-        if #colliders > 0 and player.jumpCount == 0 then
-            player:applyLinearImpulse(0, -7000)
-            player.jumpCount = player.maxJumpCount            
+        if player.grounded and player.jumpCount == 0 then
+            player:applyLinearImpulse(0, -4000)
+            player.jumpCount = player.maxJumpCount
+            player.animation = animations.jump            
         end
-        if #colliders == 0 and player.jumpCount > 0 then
+        if player.grounded == false and player.jumpCount > 0 then
             player.jumpCount = player.jumpCount - 1
-            player:applyLinearImpulse(0, -7000)
+            player:applyLinearImpulse(0, -4000)
+            player.animation = animations.jump
         end
     end
 end
+
+
+--[[ This is the default jumping from the tutorial.
+function love.keypressed(key)
+    if key == 'up' then
+        if player.grounded then
+            player:applyLinearImpulse(0,-4000)
+        end
+    end
+end
+]]
 
 --[[
     This is just a debugging function that helps show how to query for colliders.
